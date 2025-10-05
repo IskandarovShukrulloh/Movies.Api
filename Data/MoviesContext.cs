1feb310cd45f90a3d099c9f1f471ab28c9443b5d
Shukrulloh <iskandarov.sh8228@gmail.com>
2025-10-05 20:48:16 +0500

    Genres-Seeding-Removed

--- Data/MoviesContext.cs
+++ Data/MoviesContext.cs
@@ -8,18 +8,5 @@ namespace Movies.Api.Data
     {
         public DbSet<Movie> Movies => Set<Movie>();
         public DbSet<Genre> Genres => Set<Genre>();
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Genre>().HasData(
-                new { Id = 1, Name = "Adventure" },
-                new { Id = 2, Name = "Action" },
-                new { Id = 3, Name = "Drama" },
-                new { Id = 4, Name = "Comedy"},
-                new { Id = 5, Name = "Horror"},
-                new { Id = 6, Name = "Sci-Fi"},
-                new { Id = 7, Name = "Documentary"}
-            );
-        }
     }
 }
